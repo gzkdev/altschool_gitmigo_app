32e24cdc4467c67e0865e12fbe88491bbff17098
gzkdev <ezihegodswill01@gmail.com>
2022-12-22 18:25:39 +0100

    chore: configure router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Vite + React</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:username" element={<ProfilePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
