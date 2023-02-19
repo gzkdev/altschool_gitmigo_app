ad6d03b400ed259c0a7939bd03c93defa1c7befd
gzkdev <ezihegodswill01@gmail.com>
2023-02-19 22:12:20 +0100

    feature: add loader to ui

--- src/components/ProfileCard/index.jsx
+++ src/components/ProfileCard/index.jsx
@@ -1,42 +1,41 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { useProfileContext } from "../../providers/ProfileProvider";
+import Loader from "../Loader";
 import styles from "./ProfileCard.module.css";
 
 const ProfileCard = () => {
   const { data, isLoading } = useProfileContext();
-  //   console.log(data);
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <div className={styles.container}>
       <div className={styles.card}>
-        <Link to="/" data-back-link>
-          Back
-        </Link>
-        <div className={styles.cardThumbnail}>
-          <img src={data.avatar_url} alt={`${data.name}'s profile photo`} />
-        </div>
-        <div data-name>{data.name}</div>
-        <div data-username>{data.login}</div>
-        {/* <div>{data.location}</div> */}
-        {/* <div data-bio>{data.bio}</div> */}
-        <div className={styles.table}>
-          <div className={styles.row}>
-            Followers<div>{data.followers}</div>
-          </div>
-          <div className={styles.row}>
-            Following<div>{data.following}</div>
-          </div>
-          <div className={styles.row}>
-            Public Repos<div>{data.public_repos}</div>
-          </div>
-        </div>
-        <Link to={`/${data.login}/repos`} className={styles.cardLink}>
-          Repositories
-        </Link>
+        {isLoading && <Loader />}
+        {data && (
+          <>
+            <Link to="/" data-back-link>
+              Back
+            </Link>
+            <div className={styles.cardThumbnail}>
+              <img src={data.avatar_url} alt={`${data.name}'s profile photo`} />
+            </div>
+            <div data-name>{data.name}</div>
+            <div data-username>{data.login}</div>
+            <div className={styles.table}>
+              <div className={styles.row}>
+                Followers<div>{data.followers}</div>
+              </div>
+              <div className={styles.row}>
+                Following<div>{data.following}</div>
+              </div>
+              <div className={styles.row}>
+                Public Repos<div>{data.public_repos}</div>
+              </div>
+            </div>
+            <Link to={`/${data.login}/repos`} className={styles.cardLink}>
+              Repositories
+            </Link>
+          </>
+        )}
       </div>
     </div>
   );
--- src/components/RepositoriesList/index.jsx
+++ src/components/RepositoriesList/index.jsx
@@ -1,39 +1,38 @@
 import { Link } from "react-router-dom";
 import { useRepositoriesContext } from "../../providers/RepositoriesProvider";
+import Loader from "../Loader";
 import styles from "./RepositoriesList.module.css";
 
 const RepositoriesList = () => {
   const { data, error, isLoading } = useRepositoriesContext();
-  console.log(data);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <div className={styles.container}>
-      <div className={styles.wrapper}>
-        <div className={styles.header}>Public Repos</div>
-        <ul className={styles.list}>
-          {data.map((repo, _) => (
-            <li className={styles.item} key={repo.id}>
-              <Link to="#">
-                <div data-name>{repo.name}</div>
-                <div data-description></div>
-                <div className={styles.table}>
-                  <div className={styles.col}>
-                    Stars {repo.stargazers_count}
+      {isLoading && <Loader />}
+      {data && (
+        <div className={styles.wrapper}>
+          <div className={styles.header}>Public Repos</div>
+          <ul className={styles.list}>
+            {data.map((repo, _) => (
+              <li className={styles.item} key={repo.id}>
+                <Link to="#">
+                  <div data-name>{repo.name}</div>
+                  <div data-description></div>
+                  <div className={styles.table}>
+                    <div className={styles.col}>
+                      Stars {repo.stargazers_count}
+                    </div>
+                    <div className={styles.col}>Forks {repo.forks_count}</div>
+                    <div className={styles.col}>
+                      Watchers {repo.watchers_count}
+                    </div>
                   </div>
-                  <div className={styles.col}>Forks {repo.forks_count}</div>
-                  <div className={styles.col}>
-                    Watchers {repo.watchers_count}
-                  </div>
-                </div>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
